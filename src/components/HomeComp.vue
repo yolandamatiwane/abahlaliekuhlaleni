3aeb243e8c3f0abb5757aed9204587e58533da83
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-11-11 01:47:11 -0800

    :?

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -2,8 +2,7 @@
     <div class="container-fluid">
         <div class="overlay">
             <div class="card-text">
-                <h3>Welcome to</h3>
-                <h1>Abahlali Ekuhlaleni</h1>
+                <h2>Welcome to Abahlali Ekuhlaleni – Empowering Our Community Together</h2>
             </div>
         </div>
     </div>
@@ -15,16 +14,32 @@ export default {
 </script>
 <style scoped>
     .container-fluid{
-        /* background-image: url('https://abahloboekuhlaleni.org/wp-content/uploads/2023/05/255467698_244806844300322_5718746574384773261_n.jpg');
+        padding-top: 40px;
+        /* background-image: url('https://media.istockphoto.com/id/1393578696/vector/profile-silhouettes-of-african-americans.jpg?s=612x612&w=0&k=20&c=WZgdC9IeX2lvRttOOyHGGCveL16b7l-AEjuzxksEdxg='); */
+        background-image: url('https://img.freepik.com/premium-vector/group-portrait-black-people-african-american-men-women-black-history-month-cartoon-flat_637281-1450.jpg');
         background-attachment: fixed;
         background-repeat: no-repeat;
         background-position: center;
-        background-size: cover; */
-        /* background-color: #0066cc; */
-        background-color: #0e3f50;
+        background-size: cover;
+       /* background-color: #0066cc;
+        background-color: #0e3f50; */
         min-height: 100vh;
         color: #f0f0f0;
         border: none;
+    }
+    .overlay::before{
+        content: '';
+        position: absolute;
+        /* margin-top: 90px; */
+        height: calc(100% - 80px);
+        top: 0;
+        left: 0;
+        width: 100%;
+        /* height: 100%; */
+        background-color: rgba(0, 0, 0, 0.5); /* Adjust color and opacity as needed */
+        transform: skewY(-40deg); /* Adjust the angle as needed */
+        transform-origin: top left;
+        z-index: 1;
     }
     /* .overlay {
         position: absolute;
